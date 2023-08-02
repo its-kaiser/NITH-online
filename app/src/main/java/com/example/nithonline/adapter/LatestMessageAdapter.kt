87ea29de21248e4936e32cef89a1ab9a983b03c2
Himanshu Arya <stararya1234@gmail.com>
2023-08-02 15:59:40 +0530

    retrieving profile image

--- app/src/main/java/com/example/nithonline/adapter/LatestMessageAdapter.kt
+++ app/src/main/java/com/example/nithonline/adapter/LatestMessageAdapter.kt
@@ -2,9 +2,15 @@ package com.example.nithonline.adapter
 
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.appcompat.view.menu.MenuView.ItemView
 import com.example.nithonline.R
 import com.example.nithonline.model.Message
+import com.example.nithonline.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 
@@ -16,6 +22,28 @@ class LatestMessageAdapter(private val latestMessage:Message):Item<GroupieViewHo
         val ivDpLatest =viewHolder.itemView.findViewById<ImageView>(R.id.iv_dp_latest)
 
         tvLatestMessage.text=latestMessage.text
+
+        val recipientId:String
+        if(latestMessage.fromId==FirebaseAuth.getInstance().uid){
+            recipientId=latestMessage.toId
+        }
+        else{
+            recipientId=latestMessage.fromId
+        }
+        val ref=FirebaseDatabase.getInstance().getReference("/users/$recipientId")
+        //retrieving user name from database
+        ref.addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val user=snapshot.getValue(User::class.java)
+                tvUsernameLatest.text=user?.userName
+                Picasso.get().load(user?.imgUrl).into(ivDpLatest)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
     }
     override fun getLayout(): Int {
         return R.layout.latest_message_item
